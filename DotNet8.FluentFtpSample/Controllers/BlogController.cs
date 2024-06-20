5b00452e7e834e1772c3e599125c9ed40fe4c951
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-20 14:18:06 +0630

    -

--- DotNet8.FluentFtpSample/Controllers/BlogController.cs
+++ DotNet8.FluentFtpSample/Controllers/BlogController.cs
@@ -2,73 +2,72 @@
 using DotNet8.FluentFtpSample.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.FluentFtpSample.Controllers
+namespace DotNet8.FluentFtpSample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly FtpService _ftpService;
+
+    public BlogController(FtpService ftpService)
     {
-        private readonly FtpService _ftpService;
+        _ftpService = ftpService;
+    }
 
-        public BlogController(FtpService ftpService)
+    [HttpGet]
+    public async Task<IActionResult> CheckDirectoryExists(string directory)
+    {
+        try
         {
-            _ftpService = ftpService;
+            bool isExist = await _ftpService.CheckDirectoryExistsAsync(directory);
+            return Ok(isExist);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> CheckDirectoryExists(string directory)
+        catch (Exception ex)
         {
-            try
-            {
-                bool isExist = await _ftpService.CheckDirectoryExistsAsync(directory);
-                return Ok(isExist);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateDirectory(string directory)
+    [HttpPost]
+    public async Task<IActionResult> CreateDirectory(string directory)
+    {
+        try
         {
-            try
-            {
-                bool isCreateSuccessful = await _ftpService.CreateDirectoryAsync(directory);
-                return Ok(isCreateSuccessful);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            bool isCreateSuccessful = await _ftpService.CreateDirectoryAsync(directory);
+            return Ok(isCreateSuccessful);
         }
-
-        [HttpPost("UploadFile")]
-        public async Task<IActionResult> UploadFile([FromForm] BlogRequestModel requestModel)
+        catch (Exception ex)
         {
-            try
-            {
-                await _ftpService.UploadFileAsync(requestModel.File, "testing");
-                return Ok(requestModel);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
+    }
 
-        [HttpDelete]
-        public async Task<IActionResult> DeleteFile()
+    [HttpPost("UploadFile")]
+    public async Task<IActionResult> UploadFile([FromForm] BlogRequestModel requestModel)
+    {
+        try
         {
-            try
-            {
-                await _ftpService.DeleteFileAsync("/testing/profile.png");
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            await _ftpService.UploadFileAsync(requestModel.File, "testing");
+            return Ok(requestModel);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteFile()
+    {
+        try
+        {
+            await _ftpService.DeleteFileAsync("/testing/profile.png");
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }

19da013193b5e5946f22e6cf7ceb66862882b2b3
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-30 23:59:50 +0630

    Feat : Update Program.cs

---
+++ sample/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args) // entry point
+    {
+        int age = 19;
+        string firstName = "Lin Thit";
+        string lastName = "Htoo";
+
+        string fullName = firstName + " " + lastName;
+
+        // interpolation
+        Console.WriteLine($"Your name is: {fullName}");
+        //Console.WriteLine("Your full name: " + fullName);
+    }
+}
